132f97fc2bae2f2b5b1e7275db8f220c565769ea
Tan Nguyenh <tngu0235@student.monash.edu>
2023-05-11 14:35:35 +1000

    Slider mockeup

---
+++ Components/Slider.tsx
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+
+
+interface SliderProps {
+  id: string;  // Add this line
+  metric: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>, id: string) => void;  // Modify this line
+}
+
+const Slider: React.FC<SliderProps> = ({ id, metric, onChange }) => {  // Modify this line
+  const [value, setValue] = useState("3");
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+    onChange(event, id);  // Pass the id to the onChange function
+  };
+
+  const getLevelColor = (level: string): string => {
+    switch (level) {
+      case "1":
+        return "#c41e3a"; // Dark red
+      case "2":
+        return "#ffa940"; // Orange
+      case "3":
+        return "#333333"; // Yellow
+      case "4":
+        return "#52c41a"; // Green
+      case "5":
+        return "#1890ff"; // Blue
+      default:
+        return "#ffffff"; // Default (white)
+    }
+  };
+
+  const getLevelEmoji = (level: string): string => {
+    switch (level) {
+      case "1":
+        return "😔"; // Very sad
+      case "2":
+        return "😢"; // Sad
+      case "3":
+        return "😐"; // Ok
+      case "4":
+        return "😊"; // Happy
+      case "5":
+        return "😀"; // Very happy
+      default:
+        return "🎚️"; // Slider (default)
+    }
+  };
+
+  return (
+    // <div className="slider">
+    <div className="slider-container">
+      <h1>{metric}</h1>
+      <div className="slider-emoji" style={{ fontSize: "50px" }}>
+        {getLevelEmoji(value)}
+      </div>
+      <input
+        type="range"
+        min="1"
+        max="5"
+        value={value}
+        className={`slider level-${value}`}
+        onChange={handleChange}
+        style={{ background: getLevelColor(value), marginTop: "20px" }}
+      />
+      <div className="slider-scale">
+        <div className="scale-level">1</div>
+        <div className="scale-level">2</div>
+        <div className="scale-level">3</div>
+        <div className="scale-level">4</div>
+        <div className="scale-level">5</div>
+      </div>
+    </div>
+    // </div>
+  );
+};
+
+export default Slider;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,119 @@
 import prisma from '@/lib/prisma'
-import { GetStaticProps } from "next"
+import { GetStaticProps, NextPage } from 'next'
 import { User } from '@prisma/client'
+import { useState, useEffect, useRef } from 'react'
+import { Row, Col } from 'react-bootstrap'
+import Slider from '@/Components/Slider'
+import TextField from '@/Components/TextFiled'
 
-function Home(props: Props) {
-  console.log(props)
+interface Metric {
+  id: string;
+  name: string;
+  rate: number;
+}
+
+interface Props {
+  metrics: Metric[];
+}
+
+const Home: NextPage<Props> = () => {
+  const [metrics, setMetrics] = useState<Metric[]>([]);
+  // const [sliderValue, setSliderValue] = useState(1);
+  const [textFieldValue, setTextFieldValue] = useState("");
+
+
+
+
+
+
+
+  
+  //Generate dummy id for testing
+  function generateRandomString(length: number): string {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+  }
+
+  // Add dummy metric
+  const addDummyMetric = () => {
+    const dummyMetric: Metric = {
+      id: generateRandomString(10),
+      name: "Dummy Metric",
+      rate: 0,
+    };
+    setMetrics([...metrics, dummyMetric]);
+  };
+
+  // Get slider value
+  const handleSliderChange = (event: React.ChangeEvent<HTMLInputElement>, metricId: string) => {
+    const rate = parseInt(event.target.value, 10);
+    const updatedMetrics = metrics.map((metric) => {
+      if (metric.id === metricId) {
+        return { ...metric, rate };
+      }
+      return metric;
+    });
+    setMetrics(updatedMetrics);
+    console.log(metrics);
+
+  };
+  //Get textField value
+  const handleTextFieldChange = (
+    event: React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    setTextFieldValue(event.target.value);
+  };
+
+  console.log(metrics);
+
+
+  // Handle DONE button click
+  const handleDoneButtonClick = () => {
+    console.log(metrics);
+    console.log(textFieldValue);
+  };
+
   return (
-    <h1>Hello</h1>
-  )
+    <div>
+      <div className="SliderDiv">
+        {metrics.map((metric) => (
+          <div key={metric.id} className="ColSlider">
+            <Slider metric={metric.name} value={metric.rate} onChange={(event) => handleSliderChange(event, metric.id)} />
+          </div>
+        )).reduce((rows, col, index) => {
+          if (index % 3 === 0) {
+            rows.push([]);
+          }
+          rows[rows.length - 1].push(col);
+          return rows;
+        }, []).map((row, rowIndex) => (
+          <div key={rowIndex} className="SliderDiv">
+            {row}
+          </div> 
+        ))}
+      </div>
+      <TextField value={textFieldValue} onChange={handleTextFieldChange}></TextField>
+
+      <button onClick={addDummyMetric}>Add Dummy Metric</button>
+      <div>
+        <button onClick={handleDoneButtonClick} > DONE </button>
+      </div>
+    </div>
+  );
 }
-type Props = {
-  feed: User[]
-}
+
+
+
+
+
+
+
+
 
 export const getStaticProps: GetStaticProps = async () => {
   const user = await prisma.user.create({
